f4df1c4246799cc18ed9629bc385f7cadf7ff4a7
saidimu apale <saidimu@gmail.com>
2015-01-22 00:24:05 -0500

    Modified PostGIS projection to use CartoDB's helper functions.

--- src/lib/cartodb.js
+++ src/lib/cartodb.js
@@ -121,7 +121,7 @@ function process_entities(json, message)  {
   // PUBLICATIONS
   // RELEVANCES
 
-  var cartodb_query = "INSERT INTO entities (the_geom, lat, lon, url, country, place, person, nationality, date_published) VALUES (ST_SetSRID(ST_Point({lon}, {lat}),4326), {lat}, {lon}, {url}, {country}, {place}, {person}, {nationality}, {date_published})";
+  var cartodb_query = "INSERT INTO entities (the_geom, lat, lon, url, country, place, person, nationality, date_published) VALUES (CDB_LatLng({lat}, {lon}), {lat}, {lon}, {url}, {country}, {place}, {person}, {nationality}, {date_published})";
   var insert_data = {
     lat: cartodb_row.lat,
     lon: cartodb_row.lon,
