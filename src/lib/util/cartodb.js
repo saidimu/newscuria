3914656aca9f0ebee213a56fe18d0de4a173a997
saidimu apale <saidimu@gmail.com>
2015-01-26 21:46:33 -0500

    Fixed stray semicolon.

--- src/lib/util/cartodb.js
+++ src/lib/util/cartodb.js
@@ -253,7 +253,7 @@ function extract_people(people)  {
       rows.push({
         person: person.commonname || person.name,
         nationality: person.nationality || "",
-        persontype: person.persontype || "";
+        persontype: person.persontype || "",
         relevance: person.relevance,
       });
 
