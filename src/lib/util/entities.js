814bfaa5046b5746d3c061c7c199c3223f593782
saidimu apale <saidimu@gmail.com>
2015-01-29 21:30:07 -0500

    Refactoring entity-extraction and publishing to queue.

--- src/lib/util/entities.js
+++ src/lib/util/entities.js
@@ -86,17 +86,40 @@ function process_opencalais_message(json, message) {
 
 
 function extract_nlp_objects(opencalais) {
-  var entities = opencalais_config.get('entities') || {};
-  var relations = opencalais_config.get('relations') || {};
-  var topics = opencalais_config.get('topics') || {};
-  var social_tags = opencalais_config.get('socialTag') || {};
-  var language = opencalais_config.get('language') || {};
 
-  return {
-    url: opencalais.url,
-    date_published: opencalais.date_published,
-    topics: topics,
-  };
+  for(var hash in opencalais) {
+    if(opencalais.hasOwnProperty(hash))  {
+      var nlp_object = opencalais[hash];
+
+      switch(nlp_object._typeGroup)  {
+        case "entities":
+          extract_entities(nlp_object);
+          break;
+
+        case "relations":
+          extract_relations(nlp_object);
+          break;
+
+        case "topics":
+          extract_topics(nlp_object);
+          break;
+
+        case "social_tags":
+          extract_social_tags(nlp_object);
+          break;
+
+        case "language":
+          extract_language(nlp_object);
+          break;
+
+        default:
+          log.error({
+            nlp_object: nlp_object,
+          }, "new, undefined _typeGroup encountered.");
+      }//switch
+    }//if
+  }//for
+
 }//extract_nlp_objects
 
 
@@ -104,6 +127,8 @@ function extract_relations(nlp_object) {
   var _type = nlp_object._type;
   var _typeGroup = nlp_object._typeGroup;
   var _typeReference = nlp_object._typeReference;
+
+  var relations = opencalais_config.get('relations') || {};
 
 }//extract_relations
 
@@ -113,6 +138,32 @@ function extract_entities(nlp_object) {
   var _typeGroup = nlp_object._typeGroup;
   var _typeReference = nlp_object._typeReference;
 
+  var entities_config = opencalais_config.get('entities') || {};
+
+  for(var entities_key in entities_config[_type])  {
+
+    if(entities_config[_type].hasOwnProperty(entities_key))  {
+      var entity = nlp_object[entities_key];
+
+      // console.log("%s: %s", entities_key, entity);
+
+      // FIXME: iterate nested objects. Only 1-level deep for now.
+      if((typeof entity) === 'object')  {
+
+        for(var nested_key in entities_config[_type][entities_key])  {
+
+          if(entities_config[_type][entities_key].hasOwnProperty(nested_key))  {
+
+            var nested_entity = entity[0][nested_key];
+            // console.log("  %s: %s", nested_key, nested_entity);
+
+          }//if
+        }//for
+      }//if
+
+    }//if
+  }//for
+
 }//extract_entities()
 
 
@@ -120,6 +171,8 @@ function extract_topics(nlp_object) {
   var _type = nlp_object._type;
   var _typeGroup = nlp_object._typeGroup;
   var _typeReference = nlp_object._typeReference;
+
+  var topics = opencalais_config.get('topics') || {};
 
 }//extract_topics()
 
@@ -129,6 +182,8 @@ function extract_social_tags(nlp_object) {
   var _typeGroup = nlp_object._typeGroup;
   var _typeReference = nlp_object._typeReference;
 
+  var social_tags = opencalais_config.get('socialTag') || {};
+
 }//extract_social_tags()
 
 
@@ -136,6 +191,8 @@ function extract_language(nlp_object) {
   var _type = nlp_object._type;
   var _typeGroup = nlp_object._typeGroup;
   var _typeReference = nlp_object._typeReference;
+
+  var language = opencalais_config.get('language') || {};
 
 }//extract_language()
 
@@ -147,4 +204,5 @@ function publish_entities_message(entities) {
 
 module.exports = {
   start: start,
+  extract_entities: extract_entities,
 };//module.exports
