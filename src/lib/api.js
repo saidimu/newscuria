cfd8e18180a8fef6abfb7c14f0f8776a57de2cd7
saidimu apale <saidimu@gmail.com>
2015-04-16 14:22:19 -0400

    Added skeleton of a Hapi-based rest server. Also deleted stale dockerfiles

---
+++ src/lib/api.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2015  Saidimu Apale
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+'use strict';
+
+var appname = "api";
+var log = require('_/util/logging.js')(appname);
+
+var queue = require('_/util/queue.js');
+var topics = queue.topics;
+
+var get_url_metadata = require('_/util/search.js').get_url_metadata;
+
+
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+  method: 'GET',
+    path: '/{url}',
+    handler: function (request, reply) {
+      var url = encodeURIComponent(request.params.url);
+
+      get_url_metadata(url, function(response) {
+        reply(response);
+      });//get_url_metadata
+    }//handler
+});
+
+
+server.start(function () {
+    log.info({
+      info: server.info.url,
+    }, "API server listening...");
+});
