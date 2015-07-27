da565cd85e6d6eb912d0d2796d0d2887003b91a7
saidimu apale <saidimu@gmail.com>
2015-07-27 19:19:53 -0400

    Refactored Opencalais HapiJS routes export to a cleaner and saner format

--- src/lib/util/hapi-plugins/opencalais/opencalais.js
+++ src/lib/util/hapi-plugins/opencalais/opencalais.js
@@ -18,213 +18,176 @@
 
 var search = require('_/util/search.js');
 
-function get_url_summary()  {
-  var route = {
-    method: ['GET', 'POST'],
-    path: '/',
-    config: {
-      plugins: {
-        'hapi-io': '/'
-      }
-    },
-    handler: function (request, reply) {
-      var url = request.query.url || request.payload.url || undefined;
+var get_url_summary = {
+  method: ['GET', 'POST'],
+  path: '/',
+  config: {
+    plugins: {
+      'hapi-io': '/'
+    }
+  },
+  handler: function (request, reply) {
+    var url = request.query.url || request.payload.url || undefined;
 
-      search.opencalais_get_url_summary(url, function(response) {
-        reply(response);
-      });//opencalais_get_url_summary
-    }//handler
-  };//route
-
-  return route;
-}//get_url_summary
+    search.opencalais_get_url_summary(url, function(response) {
+      reply(response);
+    });//opencalais_get_url_summary
+  }//handler
+};//get_url_summary
 
 
-function get_url_tags()  {
-  var route = {
-    method: ['GET', 'POST'],
-    path: '/tags/',
-    config: {
-      plugins: {
-        'hapi-io': '/tags/'
-      }
-    },
-    handler: function (request, reply) {
-      var url = request.query.url || request.payload.url || undefined;
+var get_url_tags = {
+  method: ['GET', 'POST'],
+  path: '/tags/',
+  config: {
+    plugins: {
+      'hapi-io': '/tags/'
+    }
+  },
+  handler: function (request, reply) {
+    var url = request.query.url || request.payload.url || undefined;
 
-      search.opencalais_get_url_tags(url, function(response) {
-        reply(response);
-      });//opencalais_get_url_tags
-    }//handler
-
-  };//route
-
-  return route;
-}//get_url_tags
+    search.opencalais_get_url_tags(url, function(response) {
+      reply(response);
+    });//opencalais_get_url_tags
+  }//handler
+};//get_url_tags
 
 
-function get_url_instances()  {
-  var route = {
-    method: ['GET', 'POST'],
-    path: '/instances/',
-    config: {
-      plugins: {
-        'hapi-io': '/instances/'
-      }
-    },
-    handler: function (request, reply) {
-      var url = request.query.url || request.payload.url || undefined;
+var get_url_instances = {
+  method: ['GET', 'POST'],
+  path: '/instances/',
+  config: {
+    plugins: {
+      'hapi-io': '/instances/'
+    }
+  },
+  handler: function (request, reply) {
+    var url = request.query.url || request.payload.url || undefined;
 
-      search.opencalais_get_url_instances(url, function(response) {
-        reply(response);
-      });//opencalais_get_url_instances
-    }//handler
-  };//route
-
-  return route;
-}//get_url_instances
+    search.opencalais_get_url_instances(url, function(response) {
+      reply(response);
+    });//opencalais_get_url_instances
+  }//handler
+};//get_url_instances
 
 
-function get_url_people()  {
-  var route = {
-    method: ['GET', 'POST'],
-    path: '/people/',
-    config: {
-      plugins: {
-        'hapi-io': '/people/'
-      }
-    },
-    handler: function (request, reply) {
-      var url = request.query.url || request.payload.url || undefined;
+var get_url_people = {
+  method: ['GET', 'POST'],
+  path: '/people/',
+  config: {
+    plugins: {
+      'hapi-io': '/people/'
+    }
+  },
+  handler: function (request, reply) {
+    var url = request.query.url || request.payload.url || undefined;
 
-      search.opencalais_get_url_people(url, function(response) {
-        reply(response);
-      });//opencalais_get_url_people
-    }//handler
-  };//route
-
-  return route;
-}//get_url_people
+    search.opencalais_get_url_people(url, function(response) {
+      reply(response);
+    });//opencalais_get_url_people
+  }//handler
+};//get_url_people
 
 
-function get_url_places()  {
-  var route = {
-    method: ['GET', 'POST'],
-    path: '/places/',
-    config: {
-      plugins: {
-        'hapi-io': '/places/'
-      }
-    },
-    handler: function (request, reply) {
-      var url = request.query.url || request.payload.url || undefined;
+var get_url_places = {
+  method: ['GET', 'POST'],
+  path: '/places/',
+  config: {
+    plugins: {
+      'hapi-io': '/places/'
+    }
+  },
+  handler: function (request, reply) {
+    var url = request.query.url || request.payload.url || undefined;
 
-      search.opencalais_get_url_places(url, function(response) {
-        reply(response);
-      });//opencalais_get_url_places
-    }//handler
-  };//route
-
-  return route;
-}//get_url_places
+    search.opencalais_get_url_places(url, function(response) {
+      reply(response);
+    });//opencalais_get_url_places
+  }//handler
+};//get_url_places
 
 
-function get_url_things()  {
-  var route = {
-    method: ['GET', 'POST'],
-    path: '/things/',
-    config: {
-      plugins: {
-        'hapi-io': '/things/'
-      }
-    },
-    handler: function (request, reply) {
-      var url = request.query.url || request.payload.url || undefined;
+var get_url_things = {
+  method: ['GET', 'POST'],
+  path: '/things/',
+  config: {
+    plugins: {
+      'hapi-io': '/things/'
+    }
+  },
+  handler: function (request, reply) {
+    var url = request.query.url || request.payload.url || undefined;
 
-      search.opencalais_get_url_things(url, function(response) {
-        reply(response);
-      });//opencalais_get_url_things
-    }//handler
-  };//route
-
-  return route;
-}//get_url_things
+    search.opencalais_get_url_things(url, function(response) {
+      reply(response);
+    });//opencalais_get_url_things
+  }//handler
+};//get_url_things
 
 
-function get_url_relations()  {
-  var route = {
-    method: ['GET', 'POST'],
-    path: '/relations/',
-    config: {
-      plugins: {
-        'hapi-io': '/relations/'
-      }
-    },
-    handler: function (request, reply) {
-      var url = request.query.url || request.payload.url || undefined;
+var get_url_relations = {
+  method: ['GET', 'POST'],
+  path: '/relations/',
+  config: {
+    plugins: {
+      'hapi-io': '/relations/'
+    }
+  },
+  handler: function (request, reply) {
+    var url = request.query.url || request.payload.url || undefined;
 
-      search.opencalais_get_url_relations(url, function(response) {
-        reply(response);
-      });//opencalais_get_url_relations
-    }//handler
-  };//route
-
-  return route;
-}//get_url_relations
+    search.opencalais_get_url_relations(url, function(response) {
+      reply(response);
+    });//opencalais_get_url_relations
+  }//handler
+};//get_url_relations
 
 
-function get_url_companies()  {
-  var route = {
-    method: ['GET', 'POST'],
-    path: '/companies/',
-    config: {
-      plugins: {
-        'hapi-io': '/companies/'
-      }
-    },
-    handler: function (request, reply) {
-      var url = request.query.url || request.payload.url || undefined;
+var get_url_companies = {
+  method: ['GET', 'POST'],
+  path: '/companies/',
+  config: {
+    plugins: {
+      'hapi-io': '/companies/'
+    }
+  },
+  handler: function (request, reply) {
+    var url = request.query.url || request.payload.url || undefined;
 
-      search.opencalais_get_url_companies(url, function(response) {
-        reply(response);
-      });//opencalais_get_url_companies
-    }//handler
-  };//route
-
-  return route;
-}//get_url_companies
+    search.opencalais_get_url_companies(url, function(response) {
+      reply(response);
+    });//opencalais_get_url_companies
+  }//handler
+};//get_url_companies
 
 
-function get_url_events()  {
-  var route = {
-    method: ['GET', 'POST'],
-    path: '/events/',
-    config: {
-      plugins: {
-        'hapi-io': '/events/'
-      }
-    },
-    handler: function (request, reply) {
-      var url = request.query.url || request.payload.url || undefined;
+var get_url_events = {
+  method: ['GET', 'POST'],
+  path: '/events/',
+  config: {
+    plugins: {
+      'hapi-io': '/events/'
+    }
+  },
+  handler: function (request, reply) {
+    var url = request.query.url || request.payload.url || undefined;
 
-      search.opencalais_get_url_events(url, function(response) {
-        reply(response);
-      });//opencalais_get_url_events
-    }//handler
-  };//route
-
-  return route;
-}//get_url_events
+    search.opencalais_get_url_events(url, function(response) {
+      reply(response);
+    });//opencalais_get_url_events
+  }//handler
+};//get_url_events
 
 // export an array of routes to be imported into a HapiJS server.route() call
 module.exports = [
-  get_url_summary(),
-  get_url_tags(),
-  get_url_instances(),
-  get_url_people(),
-  get_url_places(),
-  get_url_things(),
-  get_url_relations(),
-  get_url_companies(),
-  get_url_events(),
+  get_url_summary,
+  get_url_tags,
+  get_url_instances,
+  get_url_people,
+  get_url_places,
+  get_url_things,
+  get_url_relations,
+  get_url_companies,
+  get_url_events,
 ];//module.exports
